d07cd48f3d3c7bef85dda9a0ed44ccd791c0830b
billy-price <billyprice98is@gmail.com>
2021-12-19 17:20:08 +1100

    removed CursorsGui from packager

--- metaboard_packager.lua
+++ metaboard_packager.lua
@@ -8,11 +8,9 @@ local metaBoardServer = game.ServerScriptService.MetaBoardServer
 local metaBoardPlayer = game.StarterPlayer.StarterPlayerScripts.MetaBoardPlayer
 local metaBoardCommon = game.ReplicatedStorage.MetaBoardCommon
 local boardGui = game.StarterGui.BoardGui
-local cursorsGui = game.StarterGui.CursorsGui
 
 metaBoardPlayer.Parent = metaBoardServer
 metaBoardCommon.Parent = metaBoardServer
 boardGui.Parent = metaBoardServer
-cursorsGui.Parent = metaBoardServer
 
 remodel.writeModelFile(metaBoardServer, output)
