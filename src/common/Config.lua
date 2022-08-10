535dacaa2deed851de75d91309a51d0b648b2634
Billy Price <billyprice98is@gmail.com>
2022-08-11 00:21:38 +1000

    version bump

--- src/common/Config.lua
+++ src/common/Config.lua
@@ -1,7 +1,7 @@
 local HttpService = game:GetService("HttpService")
 
 local Config = {
-	Version = "v0.12.9",
+	Version = "v0.12.10",
 	BoardTag = "metaboard",
 	BoardTagPersonal = "metaboard_personal",
 	BoardTagHistory = "metaboard_history",
