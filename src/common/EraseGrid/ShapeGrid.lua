5f67027443738de95f9ac5a20f10d8ac75716648
billy-price <billyprice98is@gmail.com>
2022-08-31 04:49:51 +0000

    added MPL 2.0 headers

--- src/common/EraseGrid/ShapeGrid.lua
+++ src/common/EraseGrid/ShapeGrid.lua
@@ -1,3 +1,9 @@
+-- --[[
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+-- --]]
+
 --[[
 	A canvas that allows for adding and removing lines and querying lines near a
 	given circle.
--- src/server/metaboardPlaceConfig.lua
+++ src/server/metaboardPlaceConfig.lua
@@ -1,3 +1,9 @@
+-- --[[
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+-- --]]
+
 --[[
 	Copy me to ServerScriptService to modify the Config file for this place.
 	Do not return anything, just modify the entries of the Config table
