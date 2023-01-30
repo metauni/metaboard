cccd1be2da5a0c46b20aab5adb28e829653dd6a0
Billy Snikkers <billyprice98is@gmail.com>
2023-01-30 11:26:09 +0100

    added license headers

--- lib/DrawingUI/Components/GuiBoardViewer/BoardViewport.lua
+++ lib/DrawingUI/Components/GuiBoardViewer/BoardViewport.lua
@@ -1,9 +1,10 @@
-local CollectionService = game:GetService("CollectionService")
 -- --[[
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 -- --]]
+
+local CollectionService = game:GetService("CollectionService")
 
 -- Imports
 local root = script.Parent.Parent.Parent.Parent
